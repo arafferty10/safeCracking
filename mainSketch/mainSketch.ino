3f4069ea3ed5b1b745f4c98f947bd6416f18fb28
lume2651 <lume2651@colorado.edu>
2018-10-08 23:08:58 -0600

    menu scaffold

--- mainSketch/mainSketch.ino
+++ mainSketch/mainSketch.ino
@@ -30,9 +30,48 @@ void setup() {
 
 void loop() {
 
+  int incoming;
+
   Serial.println();
   Serial.println();
   Serial.println("Main Menu");
+  Serial.println(F("1. "));
+  Serial.println(F("2. "));
+  Serial.println(F("3. "));
+  Serial.println(F("4. "));
+  Serial.println(F("5. "));
+  Serial.println(F("6. "));
+  Serial.println();
+  Serial.print(F("Enter your choice"));
 
+
+
+  while(!Serial.available()){
+
+    incoming = Serial.parseInt();
+
+    switch(incoming){
+      case 1:
+
+      break;
+      case 2:
+
+      break;
+      case 3:
+
+      break;
+      case 4:
+
+      break;
+      case 5:
+
+      break;
+      case 6:
+
+      break;
+      default:
+      break;
+    }
+  }
 
 }
