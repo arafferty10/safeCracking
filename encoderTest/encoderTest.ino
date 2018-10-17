1228f0d7d5bbb6c1f00c6aea43c7918a3b5bb884
lume2651 <lume2651@colorado.edu>
2018-10-16 20:42:29 -0600

    test sketch for reading in encoder values

--- encoderTest/encoderTest.ino
+++ encoderTest/encoderTest.ino
@@ -1,9 +1,43 @@
+const byte encA = 1;
+const byte encB = 3;
+
+#define CCW 0
+#define CW 1
+
+volatile int steps = 0;
+boolean dir = CW;
+boolean prevDir = CW;
+
+void countEncoder(){
+  if(dir == CW){
+    steps--;
+  } else {
+    steps++;
+  }
+  if(steps < 0){
+    steps = 8399;
+  }
+  if(steps > 8399){
+    steps = 0;
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Encoder Test");
+
+  pinMode(encA, INPUT);
+  pinMode(encB, INPUT);
+
+  // Attach encoder interrupts
+  attachInterrupt(digitalPinToInterrupt(encA), countEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encB), countEncoder, CHANGE);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  Serial.print("Steps: ");
+  Serial.println(steps);
 
 }
