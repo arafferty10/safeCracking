005c85205f533d2492e815349b353a3b99c3991d
lume2651 <lume2651@colorado.edu>
2018-11-25 17:03:13 -0700

    add basic motor control functions (on, off, turn CW/CCW) and function for detecting photogate

--- dialControl/dialControl.ino
+++ dialControl/dialControl.ino
@@ -1,4 +1,50 @@
+#define CCW 0
+#define CW 1
 
+// Pin definitions
+const int motorControl = 6; // motor PWM
+const int photoGate = 7;
+const int motorReset = 8; // motor reset pin
+const int motorDir = 3; // motor direction | HIGH - CW, LOW - CCW
+
+volatile int steps = 0;
+boolean dir = CW; // default direction (also direction for first digit of combo)
+boolean prevDir = CW;
+
+// Looks to see if the photogate has been triggered by the coupler flag
+boolean flagFound() { 
+  if( digitalRead(photoGate) == LOW ){
+    return (true);
+  }
+  return (false);
+}
+
+// Sets speed of motor, 0 - 255
+void setMotorSpeed(int speed){
+  analogWrite(motorControl, speed);
+}
+
+// Tells motor to turn clockwise
+void turnCW() {
+  dir = CW;
+  digitalWrite(motorDir, HIGH); 
+}
+
+// Tells motor to turn counter clockwise
+void turnCCW(){
+  dir = CCW;
+  digitalWrite(motorDir, LOW);
+}
+
+// Turn motor controller on
+void motorOn(){
+  digitalWrite(motorReset, HIGH);
+}
+
+// Turn motor controller off
+void motorOff(){
+  digitalWrite(motorReset, LOW);
+}
 
 
 void setup() {
